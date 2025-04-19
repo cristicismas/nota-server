e7ea0dd47b6956ceadc4adced90b83183f7d4764
Cristi Cismas <cristicismas99@gmail.com>
2025-04-19 11:12:38 +0300

    generateUser.js refactoring

--- scripts/generateUser.js
+++ scripts/generateUser.js
@@ -1,13 +1,5 @@
-import sqlite from "better-sqlite3";
-import dotenv from "dotenv";
 import bcrypt from "bcrypt";
-
-dotenv.config({});
-
-const db = new sqlite(process.env.DATABASE_URL);
-
-// Enable foreign_keys, we need to do this every time the database is opened
-db.pragma("foreign_keys = ON");
+import db from "../src/helpers/db.js";
 
 if (process.argv.length - 2 !== 2) {
   console.log(
